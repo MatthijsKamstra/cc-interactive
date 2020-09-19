056bdb68adfaf31b7505d8d0385543dc5c7570eb
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-09-19 16:51:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# cc-interactive
+
+Interactive minimal art with
+
+- https://github.com/MatthijsKamstra/cc-sketch-plus
+- https://github.com/MatthijsKamstra/cc-gamepad
+
+<p align="center">
+  <img src="icon.png" />
+</p>
+
+## Haxelib
+
+How to install cc-interactive
+
+Use this git repos as a development directory:
+
+```
+haxelib dev cc-interactive path/to/folder
+```
+
+or use this git-repository directly
+
+```
+haxelib git cc-sketch https://github.com/MatthijsKamstra/cc-interactive.git
+```
+
+don't forget to add it to your build file
+
+```
+-lib cc-interactive
+```
---
+++ src/Import.hx
@@ -0,0 +1,29 @@
+// quick and dirty import for Haxe JavaScript
+#if js
+import js.Browser.*;
+import js.html.*;
+// easy access to Globals
+import Sketcher.Globals.*;
+// easy access to the utils static funciton
+import sketcher.util.MathUtil.*;
+import sketcher.util.ColorUtil.*;
+// utils
+import sketcher.util.*;
+import sketcher.util.ColorUtil;
+import sketcher.util.GridUtil;
+import sketcher.util.MathUtil;
+// easy access to typedef
+import sketcher.AST;
+import sketcher.draw.Text;
+// animation lib
+import sketcher.lets.Go;
+import sketcher.lets.easing.*;
+// settings
+import js.Browser.*;
+import js.html.*;
+import sketcher.App;
+import js.html.*;
+import js.Browser.document;
+import js.Browser.window;
+import js.Browser.*;
+#end
